c3b0c26eb652aa74599a3b343010ef3911c036f1
dmitry.burnyshev <Igthb11>
2015-11-26 15:10:07 +0300

    project is done

--- src/mvc/Controller.java
+++ src/mvc/Controller.java
@@ -59,42 +59,57 @@ public class Controller {
         }
     }
 
-    //TODO: Private String calcCode() {}
-    public String calcCode(int date, int depNo) {
+    /**
+     * Расчет кода авторизации
+     * @param date дата ввода кода
+     * @param depNo номер подразделения
+     * @return сгенерированный код
+     */
+    private String calcCode(int date, int depNo) {
         String result;
         int iCRC;
-        int[] sKey = new int[10];
+        char[] sKey = new char[10];
+
         depNo += 1000;
         result = "" + depNo;
         result = result.substring(Math.max(0, result.length() - 3));
         result = date + result;
+
         iCRC = 0;
         for (int i = 0; i < 9; i++) {
             iCRC += Integer.parseInt(String.valueOf(result.charAt(i)));
         }
         iCRC = iCRC % 9;
-        sKey[0] = iCRC;
-        sKey[1] = Integer.parseInt(String.valueOf(result.charAt(8)));
-        sKey[2] = Integer.parseInt(String.valueOf(result.charAt(3)));
-        sKey[3] = Integer.parseInt(String.valueOf(result.charAt(1)));
-        sKey[4] = Integer.parseInt(String.valueOf(result.charAt(6)));
-        sKey[5] = Integer.parseInt(String.valueOf(result.charAt(2)));
-        sKey[6] = Integer.parseInt(String.valueOf(result.charAt(5)));
-        sKey[7] = Integer.parseInt(String.valueOf(result.charAt(0)));
-        sKey[8] = Integer.parseInt(String.valueOf(result.charAt(4)));
-        sKey[9] = Integer.parseInt(String.valueOf(result.charAt(7)));
 
-        StringBuilder builder = new StringBuilder();
-        for (int i : sKey) {
-            builder.append(i);
-        }
-        result = builder.toString();
+        sKey[0] = Integer.toString(iCRC).charAt(0);
+        sKey[1] = result.charAt(8);
+        sKey[2] = result.charAt(3);
+        sKey[3] = result.charAt(1);
+        sKey[4] = result.charAt(6);
+        sKey[5] = result.charAt(2);
+        sKey[6] = result.charAt(5);
+        sKey[7] = result.charAt(0);
+        sKey[8] = result.charAt(4);
+        sKey[9] = result.charAt(7);
+
+        result = String.valueOf(sKey);
 
         for (int i = 0; i < iCRC; i++) {
             result = result.charAt(9) + result.substring(0, 9);
         }
 
+        StringBuilder builder = new StringBuilder(result);
+        for (int i = 1; i < 10; i++) {
+            builder.setCharAt(i-1,
+                    Integer.toString((Integer.parseInt(String.valueOf(result.charAt(i-1)))
+                            + i) % 10 ).charAt(0));
+        }
+        result = builder.toString();
 
+        int threshold = Integer.parseInt(String.valueOf(result.charAt(0)));
+        for (int i = 0; i < threshold; i++) {
+            result = result.charAt(9) + result.substring(0,9);
+        }
 
         return result;
     }
@@ -108,11 +123,9 @@ public class Controller {
         switch (this.getMenuItem()) {
             case 1:
                 this.setData();
-                //TODO String result = this.calcCode(model.getDate, model.getDepNo)
-                //Заглушка
-                String testResult = "" + model.getDate() + ":" + model.getDepNo();
+                String result = this.calcCode(model.getDate(), model.getDepNo());
                 while (select == 0) {
-                    view.showResult(testResult);
+                    view.showResult(result);
                     try {
                         select = sc.nextInt();
                     } catch (Exception e) {
